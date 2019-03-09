40b7f5c7d5a04cea7efe1bdcd9d703101e5a4bde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-09 08:46:46 +0000

    Deploy malsf21/desk to github.com/malsf21/desk.git:gh-pages

---
+++ precache-manifest.048b23668423441d8a8043c33fb1f19f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21a134c4cab8a8808de4",
+    "url": "/static/js/main.21a134c4.chunk.js"
+  },
+  {
+    "revision": "c355681bf1b6d5a3288f",
+    "url": "/static/js/1.c355681b.chunk.js"
+  },
+  {
+    "revision": "21a134c4cab8a8808de4",
+    "url": "/static/css/main.43358bc5.chunk.css"
+  },
+  {
+    "revision": "981815bbb36f323d2d10cb3648f133eb",
+    "url": "/index.html"
+  }
+];
